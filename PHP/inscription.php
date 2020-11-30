056cd559cf6da5f9392f0b05ebdcaa8e0dcfe23e
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-11-30 12:48:18 +0100

    yes

--- PHP/inscription.php
+++ PHP/inscription.php
@@ -11,65 +11,65 @@
 
 		<form method="post" action="page_de_garde.php" class = "form-group">
 
-			<div id = "deux_blocs">
+			<div class = "deux_blocs">
 				<div>
-					<label for="email" id = "text_inscription_form">Email : </label>
-					<input type="email" id="input_inscription_form" name="email">
+					<label for="email" class = "text_inscription_form">Email : </label>
+					<input type="email" class = "input_inscription_form" name="email">
 					
 				</div>
 				<div>
-					<label for="pwd" id = "text_inscription_form">Mot de Passe : </label>
-					<input type="password" id="input_inscription_form" name="pwd">
+					<label for="pwd" class = "text_inscription_form">Mot de Passe : </label>
+					<input type="password" class = "input_inscription_form" name="pwd">
 				</div>
 
 				<div>
 					<br><br>
-					<label for="nom" id = "text_inscription_form">Nom : </label>
-					<input type="text" id="input_inscription_form" name="nom">
+					<label for="nom" class = "text_inscription_form">Nom : </label>
+					<input type="text" class ="input_inscription_form" name="nom">
 				</div>
 
 				<div>
-					<label for="prenom" id = "text_inscription_form">Prénom : </label>
-					<input type="text" id="input_inscription_form" name="prenom">
+					<label for="prenom" class = "text_inscription_form">Prénom : </label>
+					<input type="text" class ="input_inscription_form" name="prenom">
 				</div>
 
 
 				<div>
 					<br>
-					<label for="telephone" id = "text_inscription_form"> Numéro de Téléphone : </label>
-					<input type="text" id="input_inscription_form" name="telephone">
+					<label for="telephone" class = "text_inscription_form"> Numéro de Téléphone : </label>
+					<input type="text" class = "input_inscription_form" name="telephone">
 				</div>
 
 				<div>
-					<label for="adresse" id = "text_inscription_form"> Adresse : </label>
-					<input type="text" id="input_inscription_form" name="adresse">
+					<label for="adresse" class = "text_inscription_form"> Adresse : </label>
+					<input type="text" class = "input_inscription_form" name="adresse">
 				</div>
 
 			</div>
 
-			<div id = "deux_blocs">
-				<label for="role" id = "text_inscription_form"> Rôle souhaité : </label>
+			<div class = "deux_blocs">
+				<label for="role" class = "text_inscription_form"> Rôle souhaité : </label>
 				<div style="color:white;margin-left:20%;">
 					<div>
-						<input type="radio" id="role_choix1" name="role" value="user">
+						<input type="radio" name="role" value="user">
 						<label for="role_choix1">Simple Utilisateur</label>
 					</div>
 
 
 					<div>
-						<input type="radio" id="role_choix2" name="role" value="annotateur">
+						<input type="radio" name="role" value="annotateur">
 						<label for="role_choix2">Annotateur</label>
 					</div>
 
 					<div>
-						<input type="radio" id="role_choix3" name="role" value="validateur">
+						<input type="radio" name="role" value="validateur">
 						<label for="role_choix3">Validateur</label>
 					</div>
 				</div>
 			</div>
 
 			<?php
-				if(isset($_POST["submit_button"])) {
+				if(isset($_POST["submit_button_inscription"])) {
 					echo "Submit Button Has Been Pushed";
 					$pwd_hashed = password_hash($_POST['pwd'], PASSWORD_DEFAULT);
 					
@@ -88,7 +88,7 @@
 			?>
 
 
-			<button type="submit" name = "submit_button" class="btn btn-default" style = "font-size:1em;margin-left:60%"> Inscription </button>
+			<button type="submit" name = "submit_inscription_button" class="btn btn-default" style = "font-size:1em;margin-left:60%"> Inscription </button>
 		</form>
 
 	</body>
--- PHP/main_utilisateur.php
+++ PHP/main_utilisateur.php
@@ -1,29 +1,36 @@
 <!DOCTYPE html>
-	<link rel="stylesheet" type="text/css" href="./css_page_de_garde.css">
+	<link rel="stylesheet" type="text/css" href="css_page_de_garde.css">
 <html>
 	<body>
 		<head>
-			Main Utilisateur
+			Espace Utilisateur
+			
+			<?php echo $_POST["select_query_type"]?>
 		</head>
 
-
-			<ul id = "menu_horizontal" style="float:right;">
-				<li> <a href="espace_annotateur.php" title="Menu déroulant ici ?"> Espace Annotateur</a> </li>
-				<li> <a href="espace_validateur.php" title="Menu déroulant ici ?"> Espace Validateur</a> </li>
-				<li> <a href="page_de_garde.php" title="Déconnexion"> Déconnexion</a> </li>
+			<! Menu horizontal pour sélectionner l'interface (Utilisateur / Annotateur / Validateur)
+			Affichage en fonction des permissions + vérifier dans chaque page que l'utilisateur en cours a les droits pour accéder à cette fonctionnalité
+			>
+			<ul class = "menu_horizontal" style="float:right;">
+				<li> <a href="main_utilisateur.php">Espace Utilisateur</a> </li>
+				<li> <a href="espace_annotateur.php">Espace Annotateur</a> </li>
+				<li> <a href="espace_validateur.php">Espace Validateur</a> </li>
+				<li> <a href="page_de_garde.php" title = "Déconnexion"> Déconnexion</a> </li>
 			</ul>
 
 		<div style="height:10vh;"> </div>
-
+		
 		<main>
-			<div id = "query_menu">
-				<label for="query_menu" id = "text_inscription_form" style="font-weight: bold;">Recherche</label>
+
+				<!_____________________________________________________________________________________________________>
+			<div class = "query_menu">
+				<label class = "text_inscription_form" style="font-weight: bold;">Recherche</label>
 				<div style="height:5%;"></div>
 
-				<form method = "post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+				<form method = "post" class = "text_query_form">
 					<div style="margin-bottom:4%;">
-						<label for="query_type" id = "text_query_form">Type de recherche</label>
-						<select name="select_query_type" id = "text_query_area_form" size = "1" onchange="this.form.submit();">
+						<label class = "text_query_form">Type de recherche</label>
+						<select name= "select_query_type" class = "text_query_area_form" size = "1" onchange="this.form.submit();">
 							<option disabled selected value><?php echo $_POST["select_query_type"]?></option>
 							<option value="genome">Génome</option>
 							<option value="gene">Gène</option>
@@ -32,120 +39,133 @@
 					</div>
 				</form>
 
-				<?php if (isset($_POST["select_query_type"]) && ($_POST["select_query_type"] == 'genome')): ?>
-					<form>
+				<!_____________________________________________________________________________________________________>
+
+				<?php if(isset($_POST["select_query_type"]) && ($_POST["select_query_type"] == 'genome')): ?>
+					<form method = "post">
 						<div style = "margin-bottom:2%;">
-							<label for="nom" id = "text_query_form">Nom Génome</label>
-							<input type="text" id = "text_query_area_form" name="nom_item">
+							<label class = "text_query_form">Nom Génome</label>
+							<input class = "text_query_area_form" type = "text" name="q_genome_name">
 						</div>
 
 						<div style = "margin-bottom:4%;">
-							<label for="nom" id = "text_query_form">Espèce</label>
-							<input type="text" id = "text_query_area_form" name="species_item">
+							<label class = "text_query_form">Espèce</label>
+							<input class = "text_query_area_form" type = "text" name="q_species">
+						</div>
+						<div>
+							<label class = "text_query_form">Séquence</label>
+							<textarea class = "text_query_area_form" name="q_sequence" rows="10"> </textarea>
 						</div>
 
+
 						<div style="height:15vw;"></div>
-						<div><button type="submit" class="btn btn-default" style = "font-size: 1em;float:right;margin-right:5%;"> Recherche</button></div>
+						<div><button name = "query_genome" type="submit" class="btn btn-default" style = "font-size: 1em;float:right;margin-right:5%;"> Recherche</button></div>
 					</form>
 				<?php endif; ?>
 				
+				<!_____________________________________________________________________________________________________>
 
 				<?php if (isset($_POST["select_query_type"]) && ($_POST["select_query_type"] == 'gene')): ?>
 
-					<div id = "query_menu">
-						<form>
+					<div class = "query_menu">
+						<form method = "post">
 							<div style = "margin-bottom:2%;">
-								<label for="nom_gene" id = "text_query_form">Nom</label>
-								<input type="text" id = "text_query_area_form" name="nom_item">
+								<label class = "text_query_form">Nom</label>
+								<input class = "text_query_area_form" type = "text" name="q_gene_name">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="sigle" id = "text_query_form">Sigle</label>
-								<input type="text" id = "text_query_area_form" name="sigle_item">
+								<label class = "text_query_form">Sigle</label>
+								<input class = "text_query_area_form" type = "text" name="q_sigle">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="sigle" id = "text_query_form">Biotype</label>
-								<input type="text" id = "text_query_area_form" name="biotype">
+								<label class = "text_query_form">Biotype</label>
+								<input class = "text_query_area_form" type = "text" name="q_biotype">
 							</div>
 
 
 							<div style = "margin-bottom:2%;">
-								<label for="nom" id = "text_query_form">Nom Génome</label>
-								<input type="text" id = "text_query_area_form" name="genome_name_item">
+								<label class = "text_query_form">Nom Génome</label>
+								<input class = "text_query_area_form" type = "text" name="q_genome_name">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="nom" id = "text_query_form">Description</label>
-								<input type="text" id = "text_query_area_form" name="description">
+								<label class = "text_query_form">Description</label>
+								<input class = "text_query_area_form" type = "text" name="q_description">
 							</div>
 
 							<div style = "margin-bottom:4%;margin-left:32%;">
-								<label for="nom" id = "text_query_form">Début</label>
-								<input type="text" style = "width:22%;" name="start">
+								<label class = "text_query_form">Début</label>
+								<input type = "text" style = "width:22%;" name="q_start">
 
-								<label for="nom" id = "text_query_form">Fin</label>
-								<input type="text" style = "width:22%;" name="stop">
+								<label class = "text_query_form">Fin</label>
+								<input type = "text" style = "width:22%;" name="q_stop">
 							</div>
 
 							<div>
-								<label for="sequence" id = "text_query_form">Séquence</label>
-								<textarea id="text_query_area_form" name="sequence" rows="10"> </textarea>
+								<label class = "text_query_form">Séquence</label>
+								<textarea class = "text_query_area_form" name="q_sequence" rows="10"> </textarea>
 							</div>
 
 							<div style="height:11vw;"></div>
-							<div><button type="submit" class="btn btn-default" style = "font-size:1em;float:right;margin-right:5%;">Recherche</button></div>
+							<div><button name = "query_gene" type="submit" class="btn btn-default" style = "font-size:1em;float:right;margin-right:5%;">Recherche</button></div>
 						</form>
 					</div>
 				<?php endif; ?>
 				
+				<!_____________________________________________________________________________________________________>
+				
 				<?php if (isset($_POST["select_query_type"]) && ($_POST["select_query_type"] == 'prot')): ?>
 					<div id = "query_menu">
-						<form>
+						<form method = "post">
 							<div style = "margin-bottom:2%;">
-								<label for="nom_gene" id = "text_query_form">Nom</label>
-								<input type="text" id = "text_query_area_form" name="nom_item">
+								<label class = "text_query_form">Nom</label>
+								<input class = "text_query_area_form" type="text" name="q_rna_name">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="sigle" id = "text_query_form">Biotype</label>
-								<input type="text" id = "text_query_area_form" name="biotype">
+								<label class = "text_query_form">Biotype</label>
+								<input class = "text_query_area_form" type = "text" name="q_biotype">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="nom" id = "text_query_form">Nom Gène</label>
-								<input type="text" id = "text_query_area_form" name="gene_name">
+								<label class = "text_query_form">Nom Gène</label>
+								<input class = "text_query_area_form" type="text" name="q_prot_name">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="nom" id = "text_query_form">Nom Génome</label>
-								<input type="text" id = "text_query_area_form" name="genome_name_item">
+								<label class = "text_query_form">Nom Génome</label>
+								<input class = "text_query_area_form" type = "text" name="q_genome_name">
 							</div>
 
 							<div style = "margin-bottom:2%;">
-								<label for="nom" id = "text_query_form">Description</label>
-								<input type="text" id = "text_query_area_form" name="description">
+								<label class = "text_query_form">Description</label>
+								<input class = "text_query_area_form" type = "text" name="q_description">
 							</div>
 							
 							<div>
-								<label for="sequence" id = "text_query_form">Séquence</label>
-								<textarea id="text_query_area_form" name="sequence" rows="10"> </textarea>
+								<label class = "text_query_form">Séquence</label>
+								<textarea class = "text_query_area_form" name="q_sequence" rows="10"> </textarea>
 							</div>
 
 							<div style="height:11vw;"></div>
-							<div><button type="submit" class="btn btn-default" style = "font-size:1em;float:right;margin-right:5%;">Recherche</button></div>
+							<div><button name = "query_prot" type="submit" class="btn btn-default" style = "font-size:1em;float:right;margin-right:5%;">Recherche</button></div>
 						</form>
 					</div>
 				<?php endif; ?>
-
+				<!_____________________________________________________________________________________________________>
 			</div>
-			<div id = "scrollable_div">
+			
+			<! Résultat de la query sous forme de tableau>
+			<div class = "scrollable_div">
 				<table class="demo">
 				</table>
 
 			</div>
 
-			<div id = "scrollable_div">
+			<! Détail sur le résultat de la query sélectionné>
+			<div class = "scrollable_div">
 			</div>
 		</main>
 
