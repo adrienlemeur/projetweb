3dc5a6706f9819b90afc2309af83db810c8dbb85
adrienlemeur <adrien.le-meur@u-psud.fr>
2020-12-04 20:48:06 +0100

    on avance

--- PHP/main_utilisateur.php
+++ PHP/main_utilisateur.php
@@ -333,21 +333,25 @@
 					connect_db();
 					if(isset($_POST['primary_key'])){
 						if($_SESSION['last_query'] == 'genome'){
-							$query = "SELECT * FROM db_genome.genome as genome WHERE genome.nom_genome = " . $_POST['primary_key'] . ";" ;
-							echo $query;
+
+							$query = "SELECT * FROM db_genome.genome as genome WHERE genome.nom_genome = '" . $_POST['primary_key'] . "';" ;
+							$look_it_up = pg_query($GLOBALS['db_conn'], $query) or die ("ERROR");
+
 						}
 						if($_SESSION['last_query'] == 'gene'){
-							$query = "SELECT * FROM db_genome.cds as cds WHERE cds.nom_cds = " . $_POST['primary_key'] . ";" ;
-							echo $query;
+							$query = "SELECT * FROM db_genome.cds as cds WHERE cds.nom_cds = '" . $_POST['primary_key'] . "';" ;
+							$look_it_up = pg_query($GLOBALS['db_conn'], $query) or die ("ERROR");
+
 						}
 						if($_SESSION['last_query'] == 'prot'){
-							$query = "SELECT * FROM db_genome.prot as prot WHERE cds.nom_cds = " . $_POST['primary_key'] . ";" ;
-							echo $query;
+							$query = "SELECT * FROM db_genome.prot as prot WHERE cds.nom_cds = '" . $_POST['primary_key'] . "';" ;
+							$look_it_up = pg_query($GLOBALS['db_conn'], $query) or die ("ERROR");
 						}
-					}
-					close_db();
 
-				
+					}
+
+					pg_free_result($look_it_up);
+					close_db();
 				?>
 			</div>
 
